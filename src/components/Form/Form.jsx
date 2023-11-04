443ad4384faeba4c77255f3be563ba93e31ee4bf
Jo1ner <semdimas@gmail.com>
2023-11-04 14:40:52 +0200

    add task-2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,63 +1,59 @@
 import { FormStyle } from './Form.styled';
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class Form extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  handleSubmit = evt => {
+export const Form = ({ createContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = evt => {
     evt.preventDefault();
 
     const formData = {
-      name: this.state.name,
-      number: this.state.number,
+      name: name,
+      number: number,
     };
-    this.props.createContact(formData);
-
-    this.setState({
-      name: '',
-      number: '',
-    });
+    createContact(formData);
+    setName('');
+    setNumber('');
   };
 
-  handleInputChange = evt => {
+  const handleInputChange = evt => {
     const value = evt.target.value;
     const name = evt.target.name;
-    this.setState({
-      [name]: value,
-    });
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
 
-  render() {
-    return (
-      <div>
-        <FormStyle onSubmit={this.handleSubmit}>
-          <label>
-            <p>Name</p>
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleInputChange}
-              required
-            />
-          </label>
-          <label>
-            <p>Number</p>
-            <input
-              type="tel"
-              name="number"
-              value={this.state.number}
-              onChange={this.handleInputChange}
-              pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
-              placeholder="XXX-XX-XX"
-              required
-            />
-          </label>
-          <button type="submit">Add contact</button>
-        </FormStyle>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <FormStyle onSubmit={handleSubmit}>
+        <label>
+          <p>Name</p>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <label>
+          <p>Number</p>
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={handleInputChange}
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+            placeholder="XXX-XX-XX"
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </FormStyle>
+    </div>
+  );
+};
